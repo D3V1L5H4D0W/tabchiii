e2f68318534706b79d6ee29d69d292a9adf5ad6a
D3V1L5H4D0W <61546448+D3V1L5H4D0W@users.noreply.github.com>
2020-03-04 00:29:25 +0000

    Add files via upload

---
+++ plugin.php
@@ -0,0 +1,34 @@
+<?php
+/*
+@filmtivi
+*/
+                 $admins = [
+  351587937,
+];
+$listplugins = [
+  "ping",
+  "help",
+  "option",
+  "sendads",
+  "countchats",
+  "addall",
+  "link"
+];
+$cplug = count($listplugins) - 1;
+for($n=0; $n<=$cplug; $n++) {
+  $pluginlist = "plugins/".$listplugins[$n].".php";
+  include($pluginlist);
+}
+$data = json_decode(file_get_contents("data.json"),true);
+$markread = $data["data"]["markread"];
+$typing = $data["data"]["typing"];
+$contacts = $data["data"]["contacts"];
+$link = $data["data"]["link"];
+unlink("MadelineProto.log");
+
+
+
+
+
+
+
